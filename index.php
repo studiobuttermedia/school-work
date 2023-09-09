b5c7a0b969962f59c12c264bdc2d33e413dc5a13
Butter <55838591+studiobuttermedia@users.noreply.github.com>
2023-09-09 07:09:09 +0000

    First Website

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+echo('<h1 style="color:#FF0000;text-align:center"> Hello World ! :)) </h1>');
+echo('<br /> ');
+echo('<h2 style="color:lightcoral;text-align:center"> Hello World ! </h2>');
+echo('<br /> ');
+
+$a = 'Bạn đã sản sàng chưa ?';
+echo($a);
+
+echo('<br /> ');
+$x = 'Tạ Nguyên Khang';
+$y = '21TKĐH2.2A';
+echo('Tôi tên là '.$x.', Lớp '.$y.'');
+echo('<br /> ');
+echo('Tôi đã sẵn sàng học php');
