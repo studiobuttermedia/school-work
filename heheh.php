f5c105ef3fef66365203e3d3af6ac4fe3b7d7151
studiobuttermedia <bebosua05@yahoo.com.vn>
2023-09-16 16:20:31 +0700

    Update Auto play

--- heheh.php
+++ heheh.php
@@ -1,7 +1,7 @@
 <?php
-echo ('<h1>Never gonna give you up</h1>');
-echo ('<br/>') ;
-echo ('Never gonna give you up');
-echo ('<br/>') ;
+echo ('<h1>Never gonna give you up</h1>'.'<br/>');
+
+echo ('Never gonna give you up'.'<br/>');
+ 
 echo ('<br/>') ;
 echo ('<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?si=tSBhjqZe4x32Lqgr&amp;controls=0&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
