c36cec57f29dfca867d72ecc810ac09d509cfbfa
studiobuttermedia <bebosua05@yahoo.com.vn>
2023-09-16 15:51:22 +0700

    Add Test Page

---
+++ test.php
@@ -0,0 +1,24 @@
+<?php
+$s1 = 200 ;
+$s2 = 900 ;
+$s3 = 40 ;
+
+if ($s1 > $s2) {
+    echo $s1. " lon hon ". $s2 ;
+}
+else{
+    echo $s2. " lon hon ". $s1 ;
+}
+echo ('<br/>') ;
+
+if ($s1 > $s2) {
+    $max = $s1 ;
+}
+elseif ($s2 > $s3) {
+    $max = $s2 ;
+}
+else {
+    $max = $s3 ;
+}
+
+echo ('Max la '. $max);
